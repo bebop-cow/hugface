21bca642280a697af46585ba3969f6617d34950d
bebop-cow <tyrinfern@yahoo.com>
2026-08-26 11:46:58 -0700

    attentions

--- pipeline.py
+++ pipeline.py
@@ -1,10 +1,23 @@
-from transformers import AutoTokenizer
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+import torch
 
 # Create a sentiment-analysis pipeline using FinBERT
 tokenizer = AutoTokenizer.from_pretrained("ProsusAI/finbert")
+model = AutoModelForSequenceClassification.from_pretrained("ProsusAI/finbert", output_attentions =True)
 
-headline = "Q3 profit beats estimates; $AAPL raises guidance"
 
-# See the human-readable pieces the headline breaks into
-tokens = tokenizer.tokenize(headline)         
-print(tokens)                 
+headline = "profit beats estimates"
+inputs = tokenizer(headline, return_tensors="pt")
+
+with torch.no_grad():
+	outputs = model(**inputs)
+
+#output.attentions is a tuple:one attention tensor per layer (12 of them)
+attn = outputs.attentions 		#tuple of 12
+first_layer = attn[0]
+print(first_layer.shape)
+
+# each layer's shape is (batch, heads, tokens, tokens)
+# grab layer 0, head 0, to get one clean (tokens x tokens) grid:
+one_head = first_layer[0, 0]
+print(one_head)
