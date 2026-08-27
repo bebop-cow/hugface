eaba9290980a6e52280d617f34f0afca66796eb2
bebop-cow <tyrinfern@yahoo.com>
2026-08-27 13:17:46 -0700

    tokenizer batch test,train

--- finetune.py
+++ finetune.py
@@ -2,6 +2,9 @@ import zipfile, pandas as pd
 from datasets import Dataset
 from huggingface_hub import hf_hub_download
 from sklearn.model_selection import train_test_split
+from transformers import AutoTokenizer
+
+tokenizer = AutoTokenizer.from_pretrained("ProsusAI/finbert")
 
 
 # (keep the hf_hub_download line from before — zip_path is already set)
@@ -32,17 +35,30 @@ df["labels"] = df["label"].map(label2id)
 
 train_df, test_df = train_test_split(
 	df, 
-	test_siz=0.2,
+	test_size=0.2,
 	stratify = df["labels"],
 	random_state = 42,
 )
 
-print(train_df["labels"].value_counts(normalize = True))
-print(test_df["labels"].value_counts(normalize = True))
+def tokenizer_batch(batch):
+	return tokenizer(
+		batch["sentence"],
+		padding = True,
+		truncation = True,
+	)
 
-#converts both to HF datasets for the Trainer
-trains_ds = Dataset.from_pandas(train_df, preserve_index=False)
-test_ds = Dataset.from_pandas(test_df, preserve_index=False)
+train_tok = trains_df.map(tokenizer_batch, batched=True)
+test_tok = test_df.map(tokenizer_batch, batched=True)
+
+print(train_tok)
+print(train_tok[0].keys())
+
+# print(train_df["labels"].value_counts(normalize = True))
+# print(test_df["labels"].value_counts(normalize = True))
+
+# #converts both to HF datasets for the Trainer
+# trains_ds = Dataset.from_pandas(train_df, preserve_index=False)
+# test_ds = Dataset.from_pandas(test_df, preserve_index=False)
 
 # print(df[["label", "labels"]].head())
 # print(df["labels"].value_counts()) 
