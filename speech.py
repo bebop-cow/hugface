ebd18f6b58d1f5078b306a6311262f449a41e73a
bebop-cow <tyrinfern@yahoo.com>
2026-08-31 16:08:56 -0700

    scoreheadlines

--- speech.py
+++ speech.py
@@ -1,10 +1,20 @@
 import re
-import ntlk
+import nltk
 nltk.download("punkt_tab")
 from nltk.tokenize import sent_tokenize
+from pipeline import score_headlines
+
+#score in chunks of 32 to keep memory sane
+results = []
+for i in range(0, len(sentences), 32):
+	chunk = sentences[i:i+32]
+	results.extend(score_headlines(chunk))
+
+print(len(results)) text
+print(results[0])
 
 text = open("speech.txt").read()
-sentences = sent_tokenize(text)
+
 
 # remove any (timestamp) = parens containing digits and colons
 text = re.sub(r"\(\d[\d:]*\)", "", text)
@@ -15,6 +25,8 @@ text = re.sub(r"^[A-Z][a-zA-Z .]+:", "", text, flags=re.MULTILINE)
 # collapse the blank lines left behind
 text = re.sub(r"\n\s*\n", "\n", text).strip()
 
-print(len[sentences])
+sentences = sent_tokenize(text)
+
+print(len(sentences))
 print(sentences[:5])
 
